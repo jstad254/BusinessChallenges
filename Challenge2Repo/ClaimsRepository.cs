f4c58d0c47c519018abef5739c520fac83be9d66
jstad254 <jfollstad@olympicproductsinc.com>
2020-10-04 19:56:26 -0400

    first commit

--- Challenge2Repo/ClaimsRepository.cs
+++ Challenge2Repo/ClaimsRepository.cs
@@ -9,27 +9,22 @@ namespace Challenge2_KomodoClaimsDeptRepo
 {
     public class ClaimsRepository
     {
-        //Claims claims = new Claims();
-
         private Queue<Claims> _claimQueue = new Queue<Claims>();
-
         // Create       Add new claim
         public void AddClaims(Claims claim)
         {
             _claimQueue.Enqueue(claim);
-
         }
         // Read         List all claims in queue
         public Queue<Claims> GetClaims()
         {
             return _claimQueue;
         }
-
         // Update       no updating required
         // Delete       Delete claim from queue... dequeue?
         public void DequeueClaims(Claims claim)
         {
-            _claimQueue.Dequeue(claim);
+            _claimQueue.Dequeue();
         }
     }
 }
